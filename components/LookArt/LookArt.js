44f90c6da1a0bdd2fd9495fbad10f336c4193b2f
serafinpl <ser.burberibar@gmail.com>
2022-02-03 07:44:43 +0100

    commit changes

--- components/LookArt/LookArt.js
+++ components/LookArt/LookArt.js
@@ -7,7 +7,7 @@ const LookArt = (props) => {
 
   return (
     <React.Fragment>
-      
+      <img src={`/assets/${props.fileName}.png`} alt="Logo Anita Machura, Anity Machury" />
     </React.Fragment>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss';
 
 import SietHeader from '../components/Header/siteHeader';
+import LookArt from '../components/LookArt/LookArt';
 
 export default function Home() {
   return (
