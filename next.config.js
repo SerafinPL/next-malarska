2461a842b8e06b9094876664581017c6a90103c7
serafinpl <ser.burberibar@gmail.com>
2022-02-03 12:50:29 +0100

    prepare to export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+/* Only FOr Export*/
+
+module.exports = {
+  assetPrefix: ".",
+};
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
-import SietHeader from '../components/Header/siteHeader';
-import LookArt from '../components/LookArt/LookArt';
-import Bio from '../components/Bio/Bio';
-import ContactForm from '../components/ContactForm/ContactForm';
+import SietHeader from "../components/Header/siteHeader";
+import LookArt from "../components/LookArt/LookArt";
+import Bio from "../components/Bio/Bio";
+import ContactForm from "../components/ContactForm/ContactForm";
+import FadeInSection from "../components/FadeFunc/FadeFunc";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Anita Machura</title>
-        <meta name="description" content="Straona malarstwa Anity Mchury znakomitej artystki" />
+        <meta
+          name="description"
+          content="Straona malarstwa Anity Mchury znakomitej artystki"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <SietHeader />
+      <FadeInSection>
+        <LookArt fileName="plewbig" />
+      </FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="gnoznezjabig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="jest_superbig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="matkabig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="autoportret-z-maskamibig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="melancholikbig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="miłoscbig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="autoportretzkrolikiembig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="melancholjabig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="autoportretbig" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="wedrowiec" /></FadeInSection>
+      <FadeInSection>
+      <LookArt fileName="reminiscencje" /></FadeInSection>
+      <FadeInSection>
 
-      <SietHeader/>
+      <Bio /></FadeInSection>
+      <FadeInSection>
 
-      <LookArt fileName='plewbig'/>
-      <LookArt fileName='gnoznezjabig'/>
-      <LookArt fileName='jest_superbig'/>
-      <LookArt fileName='matkabig'/>
-      <LookArt fileName='autoportret-z-maskamibig'/>
-      <LookArt fileName='melancholikbig'/>
-      <LookArt fileName='miłoscbig'/>
-      <LookArt fileName='autoportretzkrolikiembig'/>
-      <LookArt fileName='melancholjabig'/>
-      <LookArt fileName='autoportretbig'/>
-      <LookArt fileName='wedrowiec'/>
-      <LookArt fileName='reminiscencje'/>
-
-      <Bio/>
-
-      <ContactForm/>
-     
+      <ContactForm /></FadeInSection>
+      
     </div>
-  )
+  );
 }
