dda904e1e0d4c52af35990c799c1ba8d6b0cc15d
serafinpl <ser.burberibar@gmail.com>
2022-02-09 16:12:18 +0100

    adding nice present

--- components/LookArt/LookArt.js
+++ components/LookArt/LookArt.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-import styles from './LookArt.module.scss';
-
+import styles from "./LookArt.module.scss";
 
 const LookArt = (props) => {
+  const [lookState, setLookState] = React.useState(false);
+
+  const changeLookStateHandler = (state) => {
+    setLookState(state);
+  };
 
   return (
-    <div className={styles.LookArt}>
-      <img src={`assets/${props.fileName}.png`} alt={props.alt} />
+    <div className={`${styles.LookArt} ${lookState ? styles.moved : " "}`}>
+      <div
+        className={`${styles.firstLook} ${
+          lookState ? styles.secondOpen : " "
+        }`}
+      >
+        <img
+          src={`assets/${props.fileName}.png`}
+          alt={props.alt}
+          onClick={() => changeLookStateHandler(!lookState)}
+        />
+      </div>
+      <div
+        className={`${styles.secondLook} ${
+          lookState ? styles.secondOpen : " "
+        }`}
+      ></div>
     </div>
   );
 };
