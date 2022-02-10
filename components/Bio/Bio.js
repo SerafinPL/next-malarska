6639d805a137cb7206bb696f88903ddcc752c4fb
serafinpl <ser.burberibar@gmail.com>
2022-02-10 22:34:09 +0100

    parse the content and pading elements

--- components/Bio/Bio.js
+++ components/Bio/Bio.js
@@ -1,80 +1,25 @@
 import React from "react";
 
- import styles from './Bio.module.scss'
-
+import styles from "./Bio.module.scss";
 
 const Bio = (props) => {
-
   return (
     <div className={styles.Bio}>
       <h1>Bio</h1>
 
-      <p>Śród takich pól przed laty, nad brzegiem ruczaju
-,
-Na pagórku niewielkim, we brzozowym gaju,
-Stał dwór szlachecki, z drzewa, lecz podmurowany;
-Świeciły się z daleka pobielane ściany,
-Tym bielsze, że odbite od ciemnej zieleni
-Topoli, co go bronią od wiatrów jesieni.
-Dom mieszkalny niewielki, lecz zewsząd chędogi
-,
-I stodołę miał wielką, i przy niej trzy stogi
-Użątku
-, co pod strzechą zmieścić się nie może.
-Widać, że okolica obfita we zboże,
-I widać z liczby kopic
-, co wzdłuż i wszerz smugów
-Świecą gęsto jak gwiazdy, widać z liczby pługów
-Orzących wcześnie łany ogromne ugoru
-,
-Czarnoziemne, zapewne należne do dworu,
-Uprawne dobrze na kształt ogrodowych grządek:
-Że w tym domu dostatek mieszka i porządek.
-Brama na wciąż
-otwarta przechodniom ogłasza,
-Że gościnna, i wszystkich w gościnę zaprasza.
-Właśnie dwukonną bryką wjechał młody panek
-I obiegłszy dziedziniec zawrócił przed ganek.
-Wysiadł z powozu; konie porzucone same,
-Szczypiąc trawę ciągnęły powoli pod bramę.
-We dworze pusto: bo drzwi od ganku zamknięto
-Zaszczepkami i kołkiem zaszczepki przetknięto.
-Podróżny do folwarku nie biegł sług zapytać,
-Odemknął, wbiegł do domu, pragnął go powitać.
-Dawno domu nie widział, bo w dalekim mieście
-Kończył nauki, końca doczekał nareszcie.
-Wbiega i okiem chciwie ściany starodawne
-Ogląda czule, jako swe znajome dawne.
-Też same widzi sprzęty, też same obicia,
-Z którymi się zabawiać lubił od powicia
-,
-Lecz mniej wielkie, mniej piękne niż się dawniej zdały.
-I też same portrety na ścianach wisiały:
-Tu Kościuszko
-w czamarce
-krakowskiej, z oczyma
-Podniesionymi w niebo, miecz oburącz trzyma;
-Takim był, gdy przysięgał na stopniach ołtarzów,
-Że tym mieczem wypędzi z Polski trzech mocarzów,
-Albo sam na nim padnie. Dalej w polskiej szacie
-Siedzi Rejtan
-, żałośny po wolności stracie;
-W ręku trzyma nóż ostrzem zwrócony do łona,
-A przed nim leży Fedon
-i żywot Katona
-.
-Dalej Jasiński
-, młodzian piękny i posępny;
-Obok Korsak
-, towarzysz jego nieodstępny:
-Stoją na szańcach
-Pragi, na stosach Moskali,
-Siekąc wrogów, a Praga już się wkoło pali.
-Nawet stary stojący zegar kurantowy
-W drewnianej szafie poznał, u wniścia alkowy
-;
-I z dziecinną radością pociągnął za sznurek,
-By stary Dąbrowskiego usłyszeć mazurek.</p>
+      <p>
+        Absolwentka malarstwa po Akademii Sztuk Pięknych w Katowicach. Dyplom
+        realizowany w pracowni malarstwa prof. Andrzeja Tobisa w 2021 roku. Moje
+        zainteresowania oscylują wokół psychologii, duchowości i aktualnych
+        wydarzeń. Swoją sztukę określiłabym jako swego rodzaju eksperymentalne
+        pole badań na temat ludzkiego umysłu, percepcji rzeczywistości i
+        zależności społeczno-kulturowych. Malarstwo jest dla mnie szczególnym
+        rodzajem autoekspresji, wynikającym z nadmiernej emocjonalności; to taka
+        taktyka wypracowana już w dzieciństwie, pozwalająca przetrwać- maluję,
+        więc żyję. Sztuka musi być ściśle powiązana z życiem, inaczej byłaby
+        nieautentyczna. Sztuka moim życiem, życie moją sztuką. Uprawiam
+        artystyczny ekshibicjonizm i cieszę się, że sztuka daje mi taka wolność.
+      </p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -24,64 +24,163 @@ const Home = (props) => {
           content="Code: Kuba Koder = Jakub Grzegorzek, Design: Anita Machura"
         />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400&display=swap" rel="stylesheet" /> 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <SietHeader />
       <FadeInSection>
-        <LookArt fileName="plewbig" title='Plew' > opis opis <br/> enter</LookArt>
+        <LookArt fileName="plewbig" title="Plew" >
+         
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          80x100 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="gnoznezjabig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="gnoznezjabig" title="Gnoznezja">
+        
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          100x140 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="jest_superbig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="jest_superbig" title="Jest super">
+          
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          110x140 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="matkabig" title='Plew' > opis opis <br/> enter</LookArt>
+        <LookArt fileName="matkabig" title="Matka">
+        Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          46x55 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="autoportret-z-maskamibig" title='Plew' > opis opis <br/> enter</LookArt>
+        <LookArt fileName="autoportret-z-maskamibig" title="Autoportet z maskami">
+        Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          100x70 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="melancholikbig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="melancholikbig" title="Melancholik">
+        Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          60x80 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="miłoscbig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="miłoscbig" title="Miłość">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          70x100 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="autoportretzkrolikiembig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="autoportretzkrolikiembig" title="Autoportret z królikiem">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          60x80 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="melancholjabig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="melancholjabig" title="Melancholja">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          200x140 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="autoportretbig" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="autoportretbig" title="Autoportret">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          60x50 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="wedrowiec" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="wedrowiec" title="Wędrowiec">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          100x140 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
-        <LookArt fileName="reminiscencje" title='Plew'> opis opis <br/> enter</LookArt>
+        <LookArt fileName="reminiscencje" title="Reminiscencje">
+          Z cyklu „Czarne słońca, czarna żółć”
+          <br />
+          <br />
+          140x180 cm
+          <br />
+          <br />
+          farba olejna na płótnie
+        </LookArt>
       </FadeInSection>
       <FadeInSection>
         <Bio />
       </FadeInSection>
       <FadeInSection>
-        <ContactForm formKey={props.formKey}/>
+        <ContactForm formKey={props.formKey} />
       </FadeInSection>
 
       <Footer />
     </div>
   );
-}
+};
 
 export default Home;
 
-
 export const getStaticProps = async () => {
-
-
   return {
     props: {
       formKey: process.env.NEXT_PUBLIC_ENDP,
